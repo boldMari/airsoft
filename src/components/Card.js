2ced7562467251d49cdafad7a4913d7102fdb979
boldMari <marianna@kubice.net>
2023-10-19 22:12:42 +0200

    preparing card component

---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import "./card.css"
+
+
+
+const Card = ({title = "Výchozí nadpis", description, link, cta}) => {
+    return (
+        <>
+            <div className="card mb-4 rounded-3 shadow-sm border-primary">
+                <div className="card-header py-3 text-bg-primary border-primary text-shadow">
+                    <h4 className="my-0 fw-normal">{title}</h4>
+                </div>
+                <div className="card-body">
+                    <ul className="list-unstyled mt-3 mb-4 shadow-sm">
+                        <li>{description}</li>
+                        <br/>
+                            <a href={link} className="w-50 btn btn-lg btn-primary border-primary text-shadow">{cta}</a>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,29 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "../components/Card";
 
 const Home = () => {
-  return (
-	<Container>
-		<Row>
-			<Col>
-				<h1>Home</h1>
-			</Col>
-		</Row>
-	</Container>
-  )
+	return (
+		<Container>
+			<Row>
+				<Col>
+					<h1>Home</h1>
+				</Col>
+			</Row>
+			<Row className="row-cols-1 row-cols-md-3 mb-3 text-center mt-5">
+				<Col>
+					<Card title="Jsi u nás poprvé?" description="Základní informace k první hre." link="/informace" cta="Více info"></Card>
+				</Col>
+				<Col>
+					<Card title="Ceník" description="od 100 Kč" link="/cenik" cta="Více info"></Card>
+				</Col>
+				<Col>
+					<Card title="Půjčovna" description="Vše, co potřebuješ k hre." link="/cenik" cta="Více info"></Card>
+				</Col>
+			</Row>
+		</Container>
+	)
 }
 
 export default Home
