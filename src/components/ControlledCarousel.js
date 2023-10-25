b61ac655fdf58584bc5ddc94bd83cf53814ed276
Jan Kubiče <jan@kubice.net>
2023-10-25 20:15:50 +0200

    Carousel colors fixed

--- src/components/ControlledCarousel.js
+++ src/components/ControlledCarousel.js
@@ -18,21 +18,21 @@ function ControlledCarousel() {
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
         <img src={img1} className='d-block w-100 cover' alt="img1" />
-        <Carousel.Caption>
+        <Carousel.Caption className='text-primary'>
           <h3>Vítejte v Dynamitce!</h3>
           <p>Jsme airsoftové CQB hřiště, nově přesunuto v Bohnicích.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
       <img src={img2}  className='d-block w-100 cover' alt="img2" />
-        <Carousel.Caption>
+        <Carousel.Caption className='text-primary'>
           <h3>Přijď si zahrát!</h3>
           <p>Ať už jsi začátečník nebo zkušený hráč, jsi u nás vítán.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
       <img src={img3} className='d-block w-100 cover' alt="img3" />
-        <Carousel.Caption>
+        <Carousel.Caption className='text-primary'>
           <h3>Co děláme?</h3>
           <p>Organizujeme pravidelné akce, týmové soutěže a nabízíme vybavení k pronájmu.</p>
         </Carousel.Caption>
