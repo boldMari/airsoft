5f1d78c049fd7a26e85f5cc94cdd27b23a634883
Jan Kubiče <jan@kubice.net>
2023-10-25 19:59:07 +0200

    Navbar implemented styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,10 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Navbar from './Navbar';
 
 const Layout = ({children}) => {
 
 	return (
 		<>
-			<Container>
-				<Row>
-					<Col>
-						<Navbar />
-					</Col>
-				</Row>
-			</Container>
+			<Navbar />
 			<main>
 				{children}
 			</main>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,47 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Icon from "./Icon";
+import Bullet from "../assets/images/bulletfarm.png";
+import './Navbar.css';
 
 const Navbar = () => {
 	return (
 		<>
-			<header className="my-3 bg-body-tertiary">
+			<nav class="navbar navbar-expand-lg my-3 bg-body-tertiary">
 				<Container>
-					<NavLink to="/" >
-						Úvod
-					</NavLink>
-					<NavLink to="/informace">
-						Informace
-					</NavLink>
-					<NavLink to="/cenik">
-						Ceník
-					</NavLink>
-					<NavLink to="/pravidla">
-						Pravidla
-					</NavLink>
-					<NavLink to="/contact">
-						Kontakt
-					</NavLink>
+					<Link className="navbar-brand" to="/"><img src={Bullet} alt="Bullet Farm" width="38" height="38" /></Link>
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/">Úvod</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/informace">Informace</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/cenik">Ceník</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/pravidla">Pravidla</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/contact">Kontakt</NavLink>
+						</li>
+					</ul>
+
 					<div className="d-flex">
 						<a href="https://www.facebook.com/airsoftklecany">
-							<Icon name="facebook" title="Facebook" size='2em'/>
+							<Icon name="facebook" title="Facebook" size='1.5em' className="bi text-body-secondary flex-shrink-0 me-3" />
 						</a>
 						<a href="https://www.instagram.com/bullet_farm_klecany/">
-							<Icon name="instagram" title="Instagram" size='2em'/>
+							<Icon name="instagram" title="Instagram" size='1.5em' className="bi text-body-secondary flex-shrink-0 me-3" />
 						</a>
 						<a href="mailto:bulletfarmklecany@gmail.com">
-							<Icon name="email" title="Email" size='2em'/>
+							<Icon name="email" title="Email" size='1.5em' className="bi text-body-secondary flex-shrink-0 me-3" />
 						</a>
 					</div>
 				</Container>
-			</header>
+			</nav>
 		</>
 	);
 };
