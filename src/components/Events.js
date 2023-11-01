d29a17adc539eced81c959012699ad8bf711ec3b
Jan Kubiče <jan@kubice.net>
2023-11-01 17:24:37 +0100

    Events cleanup

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,30 +1,29 @@
 import klecany from "../assets/images/klecany_.jpg"
 import burza from "../assets/images/airsoft-burza_1920.jpg"
 import dynamitka from "../assets/images/mp7fRR.jpeg"
+import { Container, Row, Col } from "react-bootstrap";
 
 const Events = () => {
 	return (
 		<>
-			<div className="container">
-				<h2 className="featurette-heading fw-normal lh-1 mt-4 mb-4 text-primary">UDÁLOSTI</h2>
-				<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-					<div className="col">
+			<Container>
+				<h2 className="featurette-heading fw-normal lh-1 my-4 text-primary">Události</h2>
+				<Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+					<Col>
 						<div className="card shadow-sm">
-							<img src={burza} alt="" className="card-img-top cover" width="100%" height="225" role="img"
-								focusable="false" />
+							<img src={burza} alt="" className="card-img-top cover" width="100%" height="225" role="img" focusable="false" />
 							<div className="card-body">
-								<h4 className="card-text">Burza</h4>
-								<p className="card-text">Naše burza je místem, kde můžete prodat své nepoužívané airsoftové vybavení a získat vybavení nové.</p>
+								<h4>Burza</h4>
+								<p>Naše burza je místem, kde můžete prodat své nepoužívané airsoftové vybavení a získat vybavení nové.</p>
 							</div>
 						</div>
-					</div>
-					<div className="col">
+					</Col>
+					<Col>
 						<div className="card shadow-sm">
-							<img src={dynamitka}className="card-img-top cover" alt="" width="100%" height="225" role="img"
-								focusable="false" />
+							<img src={dynamitka} className="card-img-top cover" alt="" width="100%" height="225" role="img" focusable="false" />
 							<div className="card-body">
-								<h4 className="card-text">First shot Dynamitka</h4>
-								<p className="card-text">Datum - první airsoftové hry v novém areálu.</p>
+								<h4>First shot Dynamitka</h4>
+								<p>První airsoftová hra v novém areálu.</p>
 								<div className="d-flex justify-content-between align-items-center">
 									<div className="btn-group">
 										<button type="button" className="btn btn-sm btn-outline-secondary">Připravujeme</button>
@@ -32,24 +31,23 @@ const Events = () => {
 								</div>
 							</div>
 						</div>
-					</div>
-					<div className="col">
+					</Col>
+					<Col>
 						<div className="card shadow-sm">
-							<img src={klecany} alt="" className="card-img-top cover" width="100%" height="225" role="img"
-								focusable="false" />
+							<img src={klecany} alt="" className="card-img-top cover" width="100%" height="225" role="img" focusable="false" />
 							<div className="card-body">
-								<h4 className="card-text">Last shot Klecany</h4>
-								<p className="card-text">25.6.2023 - se konala poslední hra v areálu Klecan.</p>
+								<h4>Last shot Klecany</h4>
+								<p>25.6.2023 - se konala poslední hra v areálu Klecan.</p>
 								<div className="d-flex justify-content-between align-items-center">
 									<div className="btn-group">
-										<button type="button" className="btn btn-sm btn-outline-secondary">Pripravujeme</button>
+										<button type="button" className="btn btn-sm btn-outline-secondary">Připravujeme</button>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>
-			</div>
+					</Col>
+				</Row>
+			</Container>
 		</>
 
 	)
