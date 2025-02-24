f81884aeb2ccb1d99253edb7ffcf1bd0251f47c0
Jan Kubiče <jan@kubice.net>
2025-02-24 17:41:33 +0100

    Fixed pricing spacing

--- src/components/PricingCard.js
+++ src/components/PricingCard.js
@@ -66,7 +66,6 @@ const PricingCard = () => {
 			<div className="container">
 				<h2 className="featurette-heading fw-normal lh-1 mt-4 pb-2 border-bottom text-primary">Vstupné</h2>
 				<div className="row row-cols-1 row-cols-md-3 mb-3 text-center mt-5">
-					
 					<div className="col">
 						<div className="card mb-4 rounded-3 shadow-sm border-primary">
 							<div className="card-header py-3 text-bg-primary border-primary">
@@ -107,165 +106,165 @@ const PricingCard = () => {
 						</div>
 					</div>
 				</div>
+			</div>
+			<div className="container">
 				<h2 className="featurette-heading fw-normal lh-1 mt-4 pb-2 border-bottom text-primary">Půjčovna</h2>
-				<div className="container">
 
-					<div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 g-3">
-						<div className="col">
-							<div className="card shadow-sm">
-								<img src={g36} className="card-img-top contain" width="100%" height="250" alt="" role="img"	focusable="false" />
-								<div className="card-body d-flex flex-column">
-									<p className="card-text">
-										Půjčujeme G36c elektrickou repliku<br/>
-										- baterie<br/>
-										- točný zásobník na 470 kuliček
-									</p>
-									<div className="mt-auto d-flex justify-content-end align-items-center">
-										<div className="btn-group">
-											<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">250 Kč/den</div>
-										</div>
+				<div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 g-3">
+					<div className="col">
+						<div className="card shadow-sm">
+							<img src={g36} className="card-img-top contain" width="100%" height="250" alt="" role="img"	focusable="false" />
+							<div className="card-body d-flex flex-column">
+								<p className="card-text">
+									Půjčujeme G36c elektrickou repliku<br/>
+									- baterie<br/>
+									- točný zásobník na 470 kuliček
+								</p>
+								<div className="mt-auto d-flex justify-content-end align-items-center">
+									<div className="btn-group">
+										<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">250 Kč/den</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div className="col">
-							<div className="card shadow-sm">
-								<img src={gloves} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
-								<div className="card-body d-flex flex-column">
-									<p className="card-text">
-										Rukavice
-									</p>
-									<div className="mt-auto d-flex justify-content-end align-items-center">
-										<div className="btn-group">
-											<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">20 Kč/den</div>
-										</div>
+					</div>
+					<div className="col">
+						<div className="card shadow-sm">
+							<img src={gloves} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
+							<div className="card-body d-flex flex-column">
+								<p className="card-text">
+									Rukavice
+								</p>
+								<div className="mt-auto d-flex justify-content-end align-items-center">
+									<div className="btn-group">
+										<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">20 Kč/den</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div className="col">
-							<div className="card shadow-sm">
-								<img src={carrier} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
-								<div className="card-body d-flex flex-column">
-									<p className="card-text">
-										Taktická vesta
-									</p>
-									<div className="mt-auto d-flex justify-content-end align-items-center">
-										<div className="btn-group">
-											<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">50 Kč/den</div>
-										</div>
+					</div>
+					<div className="col">
+						<div className="card shadow-sm">
+							<img src={carrier} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
+							<div className="card-body d-flex flex-column">
+								<p className="card-text">
+									Taktická vesta
+								</p>
+								<div className="mt-auto d-flex justify-content-end align-items-center">
+									<div className="btn-group">
+										<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">50 Kč/den</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div className="col">
-							<div className="card shadow-sm">
-								<img src={glasses} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
-								<div className="card-body d-flex flex-column">
-									<p className="card-text">
-										Balistické brýle
-									</p>
-									<div className="mt-auto d-flex justify-content-end align-items-center">
-										<div className="btn-group">
-											<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">10 Kč/den</div>
-										</div>
+					</div>
+					<div className="col">
+						<div className="card shadow-sm">
+							<img src={glasses} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
+							<div className="card-body d-flex flex-column">
+								<p className="card-text">
+									Balistické brýle
+								</p>
+								<div className="mt-auto d-flex justify-content-end align-items-center">
+									<div className="btn-group">
+										<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">10 Kč/den</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div className="col">
-							<div className="card shadow-sm">
-								<img src={mask} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
-								<div className="card-body d-flex flex-column">
-									<p className="card-text">
-										Drátěnná maska
-									</p>
-									<div className="mt-auto d-flex justify-content-end align-items-center">
-										<div className="btn-group">
-											<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">20 Kč/den</div>
-										</div>
+					</div>
+					<div className="col">
+						<div className="card shadow-sm">
+							<img src={mask} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
+							<div className="card-body d-flex flex-column">
+								<p className="card-text">
+									Drátěnná maska
+								</p>
+								<div className="mt-auto d-flex justify-content-end align-items-center">
+									<div className="btn-group">
+										<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">20 Kč/den</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div className="col">
-							<div className="card shadow-sm">
-								<img src={vest} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
-								<div className="card-body d-flex flex-column">
-									<p className="card-text">
-										Reflexní vesta<br/>Záloha 40 Kč
-									</p>
-									<div className="mt-auto d-flex justify-content-end align-items-center">
-										<div className="btn-group">
-											<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">10 Kč/den</div>
-										</div>
+					</div>
+					<div className="col">
+						<div className="card shadow-sm">
+							<img src={vest} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
+							<div className="card-body d-flex flex-column">
+								<p className="card-text">
+									Reflexní vesta<br/>Záloha 40 Kč
+								</p>
+								<div className="mt-auto d-flex justify-content-end align-items-center">
+									<div className="btn-group">
+										<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">10 Kč/den</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div className="col">
-							<div className="card shadow-sm">
-								<img src={overal} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
-								<div className="card-body d-flex flex-column">
-									<p className="card-text">
-										Overal 
-									</p>
-									<div className="mt-auto d-flex justify-content-end align-items-center">
-										<div className="btn-group">
-											<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">100 Kč/den</div>
-										</div>
+					</div>
+					<div className="col">
+						<div className="card shadow-sm">
+							<img src={overal} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
+							<div className="card-body d-flex flex-column">
+								<p className="card-text">
+									Overal 
+								</p>
+								<div className="mt-auto d-flex justify-content-end align-items-center">
+									<div className="btn-group">
+										<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">100 Kč/den</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+			</div>
 
+			<div className="container">
 				<h2 className="featurette-heading fw-normal lh-1 mt-4 pb-2 border-bottom text-primary">Prodej</h2>
-				<div className="container">
-					<div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 g-3">
-						<div className="col">
-							<div className="card shadow-sm">
-								<img src={bbs} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
-								<div className="card-body d-flex flex-column">
-									<p className="card-text">
-										BLS 0.25 g / BLS 0.28 g<br/>
-										1 kg balení
-									</p>
-									<div className="mt-auto d-flex justify-content-end align-items-center">
-										<div className="btn-group">
-											<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">230 Kč</div>
-										</div>
+				<div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 g-3">
+					<div className="col">
+						<div className="card shadow-sm">
+							<img src={bbs} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
+							<div className="card-body d-flex flex-column">
+								<p className="card-text">
+									BLS 0.25 g / BLS 0.28 g<br/>
+									1 kg balení
+								</p>
+								<div className="mt-auto d-flex justify-content-end align-items-center">
+									<div className="btn-group">
+										<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">230 Kč</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div className="col">
-							<div className="card shadow-sm">
-								<img src={co2} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
-								<div className="card-body d-flex flex-column">
-									<p className="card-text">
-										CO<sup>2</sup> bombička<br/>
-									</p>
-									<div className="mt-auto d-flex justify-content-end align-items-center">
-										<div className="btn-group">
-											<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">15 Kč</div>
-										</div>
+					</div>
+					<div className="col">
+						<div className="card shadow-sm">
+							<img src={co2} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
+							<div className="card-body d-flex flex-column">
+								<p className="card-text">
+									CO<sup>2</sup> bombička<br/>
+								</p>
+								<div className="mt-auto d-flex justify-content-end align-items-center">
+									<div className="btn-group">
+										<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">15 Kč</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div className="col">
-							<div className="card shadow-sm">
-								<img src={greengas} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
-								<div className="card-body d-flex flex-column">
-									<p className="card-text">
-										GreenGas Ultrair
-									</p>
-									<div className="mt-auto d-flex justify-content-end align-items-center">
-										<div className="btn-group">
-											<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">200 Kč</div>
-										</div>
+					</div>
+					<div className="col">
+						<div className="card shadow-sm">
+							<img src={greengas} className="card-img-top contain" width="100%" height="250" alt="" role="img" focusable="false" />
+							<div className="card-body d-flex flex-column">
+								<p className="card-text">
+									GreenGas Ultrair
+								</p>
+								<div className="mt-auto d-flex justify-content-end align-items-center">
+									<div className="btn-group">
+										<div className="border border-secondary rounded-3 text-body-secondary fs-4 px-2 py-1 btn btn-sm btn-outline-secondary">200 Kč</div>
 									</div>
 								</div>
 							</div>
