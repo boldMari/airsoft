0f55c7d96858529edec2691e6c892f15acbcba4a
Jan Kubiče <jan@kubice.net>
2025-02-27 18:35:05 +0100

    Added bullet photos to carousel

--- src/components/ControlledCarousel.js
+++ src/components/ControlledCarousel.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import "./ControlledCarousel.css"
 import Carousel from 'react-bootstrap/Carousel';
-import img1 from '../assets/images/military_1920.jpg'
-import img2 from '../assets/images/military-g385_1920.jpg'
-import img3 from '../assets/images/airsoft-replica_1920.jpg'
+import img1 from '../assets/images/bullet_dron.jpg'
+import img2 from '../assets/images/bullet_box.jpg'
+import img3 from '../assets/images/military_1920.jpg'
+// import img2 from '../assets/images/military-g385_1920.jpg'
+// import img3 from '../assets/images/airsoft-replica_1920.jpg'
 
 
 
