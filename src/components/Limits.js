893abca8f2005cf3b8e5dcf891233811b2c092bf
Jan Kubiče <jan@kubice.net>
2025-05-23 11:30:07 +0200

    Limits updated

--- src/components/Limits.js
+++ src/components/Limits.js
@@ -7,12 +7,16 @@ const Limits = () => {
 
 	const limits = {
 		saturday: {
-			aeg: "1,7 J",
-			sniper: "2 J",
+			safe: "1,7 J",
+			strong: "2 J",
+			sniper: "2,5 J",
+			pistol: "1,7 J"
 		},
 		sunday: {
-			aeg: "2 J",
-			sniper: "2,5 J",
+			safe: "1,7 J",
+			strong: "2,3 J",
+			sniper: "3 J",
+			pistol: "1,7 J"
 		}
 	}
 
@@ -30,27 +34,39 @@ const Limits = () => {
 
 	return (
 		<>
-			<div className="container px-4 py-5" id="icon-grid">
+			<div className="py-2" id="icon-grid">
 				<h2 className="pb-2 border-bottom text-primary">Limity</h2>
 			</div>
 			<div className="text-end">
-			<span className={toggled ? 'text-primary' : ''}><b>Sobota</b></span> <Toggle onChange={buttonHandler}></Toggle> <span className={!toggled ? 'text-primary' : ''}>Neděle</span>
-				{/* <p>Změna limitů : {toggled ? "Neděle" : "Sobota"}</p> */}
+				<span className={toggled ? 'text-primary' : ''}><b>Sobota</b></span> <Toggle onChange={buttonHandler}></Toggle> <span className={!toggled ? 'text-primary' : ''}>Neděle 18+</span>
 			</div>
 			<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4 py-5">
 				<div className="col d-flex align-items-start">
-					<Icon name="ak47" title="Granade" size='2em' className="bi text-body-secondary flex-shrink-0 me-3" width="50px" height="50px" />
+					<Icon name="pistol" title="Pistole" size='2em' className="bi text-body-secondary flex-shrink-0 me-3" width="50px" height="50px" />
 					<div>
-						<h3 className="fw-bold mb-0 fs-4 text-body-emphasis">AEG/HPA/plyn <span className="text-primary">{dynamicLimits.aeg}</span></h3>
-						<p>(117m/s s kuličkami 0,25g; plynovky a HPA měříme na používaném střelivu, ostatní na 0,25g).</p>
+						<h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Pistole <span className="text-primary">{dynamicLimits.pistol}</span></h3>
+						<p>AEG/GBB/HPA max BB 0,36</p>
+					</div>
+				</div>
+				<div className="col d-flex align-items-start">
+					<Icon name="ak47" title="Safe gun" size='2em' className="bi text-body-secondary flex-shrink-0 me-3" width="50px" height="50px" />
+					<div>
+						<h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Safe gun <span className="text-primary">{dynamicLimits.safe}</span></h3>
+						<p>AEG/GBB/HPA max BB 0,36 / full auto nad 20m / v&nbsp;budovách pouze semi</p>
+					</div>
+				</div>
+				<div className="col d-flex align-items-start">
+					<Icon name="ak47" title="Safe gun" size='2em' className="bi text-body-secondary flex-shrink-0 me-3" width="50px" height="50px" />
+					<div>
+						<h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Strong gun <span className="text-primary">{dynamicLimits.strong}</span></h3>
+						<p>AEG/GBB/HPA max BB 0,36 / nutnost PISTOLE nebo SAFE GUN pod 20m</p>
 					</div>
 				</div>
 				<div className="col d-flex align-items-start">
 					<Icon name="sniper" title="Sniper" size='2em' className="bi text-body-secondary flex-shrink-0 me-3" width="50px" height="50px" />
 					<div>
-						<h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Opakovací pušky <span className="text-primary">{dynamicLimits.sniper}</span></h3>
-						<p>(manuální/plynové/HPA pušky; 126m/s s kuličkami 0.25g) minimální vzdálenost pro střelbu je 𝟭𝟱 𝗺 a hráč je
-							povinen mít záložní zbraň, splňující limit 1.7J.</p>
+						<h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Odstřelovací puška <span className="text-primary">{dynamicLimits.sniper}</span></h3>
+						<p>manuální/HPA/GBB / max BB 0,48 / nutnost PISTOLE nebo SAFE GUN pod 20m</p>
 					</div>
 				</div>
 				<div className="col d-flex align-items-start">
